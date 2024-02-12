1898a3aaf96f9f29958d2dd13d3392495d857585
Almustapha Muhammad Sunusi <mustaphasunusi5@gmail.com>
2024-02-12 01:37:37 +0100

    made some changeson the hero

--- src/components/hero.vue
+++ src/components/hero.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex flex-col md:flex-row items-center w-100 md:h-[50dvh] h-[90dvh] lg:h-[90dvh] bg-main px-10 md:px-20 pt-10 md:pt-0">
+    <div class="flex flex-col md:flex-row items-center w-100 md:h-[50dvh] h-auto lg:h-[90dvh] bg-main px-10 md:px-20 pt-10 md:pt-0">
         <section class="flex-1">
            <h2 class="lg:text-[3.3rem] md:text-3xl text-2xl lg:leading-[1.4] text-white font-semibold animate__animated animate__fadeInLeft ">Towards a Sustainable Financial and Economic 
                 <span class="text-btn">
@@ -8,12 +8,12 @@
             </h2>
             <p class="text-secondary lg:text-[2.1rem] lg:leading-[1.2] py-10 md:text-xl animate__animated animate__fadeInLeft animate__delay-1s">Access loans at 0% interest rate upon repayment</p>
             <button
-              class=" px-8 py-3 text-white text-sm rounded-md shadow shadow-yellow-600 font-semibold bg-btn animate__animated animate__fadeInRight animate__delay-1s"
+              class=" px-3 py-3 text-white text-sm rounded-md shadow shadow-yellow-600 font-semibold bg-btn animate__animated animate__fadeInRight animate__delay-1s"
             >
             Seed sale and Initial NFT Offering is live. Click here for more     
         </button>
         </section>
-        <section class="flex-1 mt-10 md:mt-0">
+        <section class="flex-1 my-10 md:mt-0">
             <!-- <img src="@/assets/dots.png"> -->
             <!-- <img src="@/assets/dot.png" > -->
             <img src="@/assets/AllSpin.gif" class="md:w-[35rem] max-h-[30rem]  animate__animated animate__fadeIn animate__delay-2s">
